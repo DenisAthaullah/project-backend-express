f7ca29f786366509d691a566f27ae7777e26fc2d
Denisath <denisathaullahdenis@gmail.com>
2025-10-06 13:14:56 +0700

    membuat method get dan post

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+//Dummy data
+let users = [
+    { id: 1, name: "ucup" },
+    { id: 2, name: "rucup" },
+];
+
+//method get
+app.get("/users", (req, res) =>{
+    res.json(users);
+});
+
+//method post
+app.post("/users", (req, res) =>{
+    const { name } = req.body;
+    const newUser = { id: users.length + 1, name:name };
+    users.push(newUser)
+    res.status(201).json(newUser);
+});
+
 
 app.get("/tes", (req, res) => {
     res.send("selamat datang di sistem saya...");
